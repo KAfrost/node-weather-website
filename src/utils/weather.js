0b335e96231b107b695eac0611b85ed8315f7275
Kristan Frost <kristan.frost@gmail.com>
2021-12-07 11:09:47 -0600

    Add humidity to forecast

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -9,7 +9,7 @@ const weather  = (lat, long, callback) => {
         } else if (body.error){
             callback("Unable to find location", undefined);
         }else {
-            callback(undefined, "The current conditions are " + body.current.weather_descriptions[0] + ". It is currently " +  body.current.temperature + " degrees. And it feels like " + body.current.feelslike + " degrees.");
+            callback(undefined, "The current conditions are " + body.current.weather_descriptions[0] + ". It is currently " +  body.current.temperature + " degrees. And it feels like " + body.current.feelslike + " degrees. With a humidity of " + body.current.humidity + "%.");
         }
     });
 };
